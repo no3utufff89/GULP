b7db5aa20064562039dcf95322f5564863fb449c
no3utufff89 <14198908@mail.ru>
2025-09-09 16:06:57 +0300

    first

---
+++ gulp/config/path.js
@@ -0,0 +1,44 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = process.env.npm_package_name;
+// const cleanFolder = [`./dist/{css,js,img}`, `./dist/*.*`];
+const srcFolder = `./src`;
+const docsFolder = './docs';
+export const path = {
+    build: {
+        js: `${buildFolder}/js/`,
+        jsFiles: `${buildFolder}/js/assets`,
+        css: `${buildFolder}/css/`,
+        html: `${buildFolder}/`,
+        images: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
+        files: `${buildFolder}/files/`,
+    },
+    src: {
+        js: `${srcFolder}/js/app.js`,
+        jsFiles: `${srcFolder}/js/assets/*.js`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+        html: `${srcFolder}/*.{html,pug,php,ico}`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+        imagesVariants: `${srcFolder}/img/`,
+        svg: `${srcFolder}/img/**/*.svg`,
+        svgicons: `${srcFolder}/icons/*.svg`,
+        files: `${srcFolder}/files/**/*.*`,
+    },
+    watch: {
+        js: `${srcFolder}/js/**/*.js`,
+        jsFiles: `${srcFolder}/js/assets/*.js`,
+        html: `${srcFolder}/**/*.{html,pug,php}`,
+        images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp,svg}`,
+        svgicons: `${srcFolder}/icons/*.svg`,
+        files: `${srcFolder}/files/**/*.*`,
+        scss: `${srcFolder}/scss/**/*.scss`,
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    docsFolder: docsFolder,
+    ftp: `httpdocs`,
+};
